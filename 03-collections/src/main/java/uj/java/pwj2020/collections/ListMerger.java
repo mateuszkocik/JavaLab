e6086f08c8a1786cecd8a5ad3d78d93490170244
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-10-24 17:57:47 +0200

    ListMerger class passes tests

--- 03-collections/src/main/java/uj/java/pwj2020/collections/ListMerger.java
+++ 03-collections/src/main/java/uj/java/pwj2020/collections/ListMerger.java
@@ -1,10 +1,28 @@
 package uj.java.pwj2020.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListMerger {
     public static List<Object> mergeLists(List<?> l1, List<?> l2) {
-        return null;
+        List<Object> list = new ArrayList<>();
+
+        if(l1 == null && l2 == null) return list;
+        else if(l1 == null) list = List.copyOf(l2);
+        else if(l2 == null) list = List.copyOf(l1);
+        else bothListsNotNull(l1, l2, list);
+
+        final List<Object> mergedList = List.copyOf(list);
+        return mergedList;
+    }
+
+    private static void bothListsNotNull(List<?> l1, List<?> l2, List<Object> list) {
+        var i1 = l1.iterator();
+        var i2 = l2.iterator();
+        while(i1.hasNext() || i2.hasNext()){
+            if(i1.hasNext()) list.add(i1.next());
+            if(i2.hasNext()) list.add(i2.next());
+        }
     }
 
 }
