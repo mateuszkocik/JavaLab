79bc262564fbc54fe3edc228f44eb7efbb7563a7
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-10-26 15:29:43 +0100

    JsonMapperImpl passes tests, commit before refactor

--- 03-collections/src/main/java/uj/java/pwj2020/collections/JsonMapperImpl.java
+++ 03-collections/src/main/java/uj/java/pwj2020/collections/JsonMapperImpl.java
@@ -1,12 +1,61 @@
 package uj.java.pwj2020.collections;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Formatter;
+import java.util.List;
 import java.util.Map;
 
 public class JsonMapperImpl implements JsonMapper {
 
     @Override
     public String toJson(Map<String, ?> map) {
-        return null;
+        String a = m(map);
+        return a;
     }
+
+    private String m(Map<String, ?> map) {
+        if(map == null || map.size() == 0) return "{}";
+
+        String result = "{";
+
+        for(Map.Entry<String,?> entry : map.entrySet()){
+            String key = entry.getKey();
+            var value = entry.getValue();
+            result += "\"" + key + "\": ";
+            result += getJsonFromObject(key,value);
+            result += ",";
+        }
+
+        result = result.substring(0,result.length()-1);
+        result += "}";
+
+        return result;
+    }
+
+    private String getJsonFromObject(String key, Object value){
+        String result = "";
+        if(value instanceof List){
+            result += "[" ;
+            for(Object o : (List)value){
+                result += getJsonFromObject(key,o) + ",";
+            }
+            if( ((List<?>) value).size() > 0) result = result.substring(0,result.length()-1);
+
+            result += "]";
+        }else if(value instanceof Map){
+            result += m((Map)value);
+        }else if(value instanceof String){
+            value = ((String)value).replace("\"", "\\\"");
+            result += "\"" + value + "\"";
+        }else{
+            result += value.toString();
+        }
+
+        return result;
+    }
+
+
+    
 
 }
