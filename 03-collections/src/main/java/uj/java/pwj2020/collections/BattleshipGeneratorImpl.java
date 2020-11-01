f3d9e99a88182a9ab35543ca291c1be82bd51cba
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-11-01 22:00:45 +0100

    Refactor code and add corner operations

--- 03-collections/src/main/java/uj/java/pwj2020/collections/BattleshipGeneratorImpl.java
+++ 03-collections/src/main/java/uj/java/pwj2020/collections/BattleshipGeneratorImpl.java
@@ -10,18 +10,10 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
     int doubleMast = 3;
     int tripleMast = 2;
     int quadrupleMast = 1;
-    List<Point> map = Point.makeListWithWaterPoints(mapSize);
+    List<Point> map = makeListWithWaterPoints(mapSize);
     List<Point> leftFields;
     static final char shipChar = '#';
     static final char waterChar = '.';
-
-    public static void main(String[] args){
-        BattleshipGeneratorImpl b = new BattleshipGeneratorImpl();
-        b.initLeftFields();
-
-        b.generateMap();
-        b.displayMap();
-    }
 
 
     private void initLeftFields(){
@@ -33,34 +25,46 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
 
     @Override
     public String generateMap(){
+        initLeftFields();
         while(thereAreLeftShips()){
+            int nextShipSize;
             ArrayList<Point> list;
             if(quadrupleMast > 0){
-                list = generateRandomXPoints(4);
-                for(Point p : list){
-
-                }
-                if(list.size() == 4){
-                    changeFromListToShips(list);
-                    deleteFromLeftFields(list);
-                }
-            quadrupleMast--;
+                nextShipSize = 4;
             }else if(tripleMast > 0){
-                tripleMast--;
+                nextShipSize = 3;
             }else if(doubleMast > 0){
-                doubleMast--;
+                nextShipSize = 2;
             }else{
-                singleMast--;
+                nextShipSize = 1;
             }
-            break;
-
+            list = generateRandomXPoints(nextShipSize);
+            if(list.size() == nextShipSize){
+                changeFromListToShips(list);
+                deleteFromLeftFields(list);
+                makeUnavailable(list);
+                if(list.size() == 1) singleMast--;
+                if(list.size() == 2) doubleMast--;
+                if(list.size() == 3) tripleMast--;
+                if(list.size() == 4) quadrupleMast--;
+            }
         }
 
-        return "a";
+        return convertMapToString();
 
     }
 
-    private boolean thereAreLeftShips() {
+    private void makeUnavailable(ArrayList<Point> list){
+        for(Point p: list){
+            var neighbours = getNeighbours(p);
+            var corners = getCornerPoints(p);
+
+            leftFields.removeAll(neighbours);
+            leftFields.removeAll(corners);
+        }
+    }
+
+    private boolean thereAreLeftShips(){
         return !(singleMast == 0 && doubleMast == 0 && tripleMast == 0 && quadrupleMast == 0);
     }
 
@@ -89,8 +93,6 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
     }
 
     private void deleteFromLeftFields(ArrayList<Point> list){
-        /*System.out.println("lista " + list.toString());
-        leftFields.removeAll(list);*/
         leftFields.removeAll(list);
     }
 
@@ -101,19 +103,14 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
     }
 
     private Point getRandomPointFromList(List<Point> list){
-        int index = (int)(Math.random()*list.size());
-        return list.get(index);
+        return list.get((int)(Math.random()*list.size()));
     }
 
     private void addNeighboursToAvailable(List<Point> availablePoints, List<Point> randomPoints, Point point){
         List<Point> temp = getNeighbours(point);
-        System.out.println("Temp: " + temp);
         for(Point p : temp){
             if(!availablePoints.contains(p) && !randomPoints.contains(p)) availablePoints.add(p);
         }
-
-        System.out.println("Available: " + availablePoints.toString());
-        System.out.println("Random: " + randomPoints.toString());
     }
 
     private List<Point> getNeighbours(Point point){
@@ -122,6 +119,16 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
         if(getRightPoint(point) != null) list.add(getRightPoint(point));
         if(getTopPoint(point) != null) list.add(getTopPoint(point));
         if(getBottomPoint(point) != null) list.add(getBottomPoint(point));
+
+        return list;
+    }
+
+    private List<Point> getCornerPoints(Point point){
+        List<Point> list = new ArrayList<>();
+        if(getTopLeftPoint(point) != null) list.add(getTopLeftPoint(point));
+        if(getTopRightPoint(point) != null) list.add(getTopRightPoint(point));
+        if(getBottomRightPoint(point) != null) list.add(getBottomRightPoint(point));
+        if(getBottomLeftPoint(point) != null) list.add(getBottomLeftPoint(point));
 
         return list;
     }
@@ -162,12 +169,47 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
         return getPoint(x.x,x.y-1);
     }
 
+    private Point getTopLeftPoint(Point x){
+        return getPoint(x.x-1,x.y+1);
+    }
+
+    private Point getTopRightPoint(Point x){
+        return getPoint(x.x+1,x.y+1);
+    }
+
+    private Point getBottomLeftPoint(Point x){
+        return getPoint(x.x-1,x.y-1);
+    }
+
+    private Point getBottomRightPoint(Point x){
+        return getPoint(x.x+1,x.y-1);
+    }
+
 
     private Point getPoint(int x, int y){
         if(x >= 0 && x < mapSize && y >= 0 && y < mapSize){
             return map.get(x*mapSize+y);
         }
         return null;
+    }
+
+    private ArrayList<Point> makeListWithWaterPoints(int size) {
+        var list = new ArrayList<Point>();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                list.add(new Point(i, j, '.'));
+            }
+        }
+        return list;
+    }
+
+    private String convertMapToString(){
+        StringBuilder sb = new StringBuilder();
+        for(Point p : map){
+            sb.append(p.sign);
+        }
+
+        return sb.toString();
     }
 
     private void displayMap(){
@@ -179,4 +221,33 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
         }
     }
 
+    class Point {
+        final int x, y;
+        char sign;
+
+        Point(int x, int y, char sign) {
+            this.x = x;
+            this.y = y;
+            this.sign = sign;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Point point = (Point) o;
+
+            if (x != point.x) return false;
+            return y == point.y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ")";
+        }
+
+    }
+
 }
+
