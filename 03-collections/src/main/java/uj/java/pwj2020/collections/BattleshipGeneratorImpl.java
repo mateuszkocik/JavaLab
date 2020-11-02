355ac7ddbbaedb1ebe1bb23d2c27b4434ef00410
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-11-02 01:13:56 +0100

    Refactor BattleshipGeneratorImpl.java

--- 03-collections/src/main/java/uj/java/pwj2020/collections/BattleshipGeneratorImpl.java
+++ 03-collections/src/main/java/uj/java/pwj2020/collections/BattleshipGeneratorImpl.java
@@ -6,117 +6,98 @@ import java.util.List;
 public class BattleshipGeneratorImpl implements BattleshipGenerator{
 
     static final int mapSize = 10;
+    static final char shipChar = '#';
+
     int singleMast = 4;
     int doubleMast = 3;
     int tripleMast = 2;
     int quadrupleMast = 1;
     List<Point> map = makeListWithWaterPoints(mapSize);
     List<Point> leftFields;
-    static final char shipChar = '#';
-    static final char waterChar = '.';
 
-
-    private void initLeftFields(){
-        leftFields = new ArrayList<Point>();
-        for(Point p : map){
-            leftFields.add(p);
-        }
-    }
 
     @Override
     public String generateMap(){
         initLeftFields();
-        System.out.println("Rozmiar: " + leftFields.size());
         while(thereAreLeftShips()){
-            int nextShipSize;
-            ArrayList<Point> list;
-            if(quadrupleMast > 0){
-                nextShipSize = 4;
-            }else if(tripleMast > 0){
-                nextShipSize = 3;
-            }else if(doubleMast > 0){
-                nextShipSize = 2;
-            }else{
-                nextShipSize = 1;
-            }
-            System.out.println("Rozmiar kolejnego statku: " + nextShipSize);
-            list = generateRandomXPoints(nextShipSize);
-            if(list.size() == nextShipSize){
-
-                changeFromListToShips(list);
-                makeUnavailable(list);
-                if(list.size() == 1) singleMast--;
-                if(list.size() == 2) doubleMast--;
-                if(list.size() == 3) tripleMast--;
-                if(list.size() == 4) quadrupleMast--;
-            }
-            displayMap();
-            System.out.println("Pozostale: " + leftFields.size() + leftFields);
+            int nextShipSize = getNextShipSize();
+            ArrayList<Point> list = generateRandomXPoints(nextShipSize);
+            changeFromListToShips(list);
+            makeUnavailable(list);
+            reduceShipNumber(list.size());
         }
-
-
 
         return convertMapToString();
-
-    }
-
-    private void makeUnavailable(ArrayList<Point> list){
-        for(Point p: list){
-
-            var neighbours = getNeighbours(p);
-            var corners = getCornerPoints(p);
-
-            leftFields.removeAll(list);
-            leftFields.removeAll(neighbours);
-            leftFields.removeAll(corners);
-
-        }
     }
 
     private boolean thereAreLeftShips(){
         return !(singleMast == 0 && doubleMast == 0 && tripleMast == 0 && quadrupleMast == 0);
     }
 
+    private int getNextShipSize(){
+        int nextShipSize;
+        if(quadrupleMast > 0){
+            nextShipSize = 4;
+        }else if(tripleMast > 0){
+            nextShipSize = 3;
+        }else if(doubleMast > 0){
+            nextShipSize = 2;
+        }else{
+            nextShipSize = 1;
+        }
+        return nextShipSize;
+    }
+
+    private void reduceShipNumber(int listSize){
+        switch(listSize){
+            case 1 : singleMast--; break;
+            case 2 : doubleMast--; break;
+            case 3 : tripleMast--; break;
+            case 4 : quadrupleMast--; break;
+        }
+    }
+
+    private void makeUnavailable(ArrayList<Point> list){
+        for(Point p : list){
+            var neighbours = getNeighbours(p);
+            var corners = getCornerPoints(p);
+
+            leftFields.removeAll(list);
+            leftFields.removeAll(neighbours);
+            leftFields.removeAll(corners);
+        }
+    }
 
     private ArrayList<Point> generateRandomXPoints(int x){
         var availablePoints = new ArrayList<Point>();
         var randomPoints = new ArrayList<Point>();
 
+        Point point = getRandomPointFromLeftFields();
 
-        Point firstPoint = getRandomPointFromLeftFields(randomPoints);
-        randomPoints.add(firstPoint);
-        addNeighboursToAvailable(availablePoints,randomPoints,firstPoint);
-        System.out.println("First point " + firstPoint);
-        System.out.println("Available points: " + availablePoints);
-        for(int i = 1; i < x; i++){
-            if(availablePoints.size() > 0){
-                Point p = getRandomPointFromList(availablePoints);
-                System.out.println(i + "   " + p);
-                randomPoints.add(p);
-                availablePoints.remove(p);
-                addNeighboursToAvailable(availablePoints,randomPoints,p);
-                System.out.println("Available points: " + availablePoints);
-
-            }
-        }
+        do{
+            randomPoints.add(point);
+            availablePoints.remove(point);
+            addNeighboursToAvailable(availablePoints, randomPoints, point);
+            point = getRandomPointFromList(availablePoints);
+            x--;
+        }while(x > 0 && availablePoints.size()>0);
 
         return randomPoints;
     }
 
     private void changeFromListToShips(List<Point> list){
-        for(Point p : list){
-            if(p != null) p.sign = shipChar;
-        }
+        for(Point p : list) p.sign = shipChar;
     }
 
     private Point getRandomPointFromList(List<Point> list){
-        return list.get((int)(Math.random()*list.size()));
+        return list.isEmpty() ? null : list.get((int) (Math.random() * list.size()));
     }
 
     private void addNeighboursToAvailable(List<Point> availablePoints, List<Point> randomPoints, Point point){
         List<Point> temp = getNeighbours(point);
         for(Point p : temp){
-            if(!availablePoints.contains(p) && !randomPoints.contains(p) && leftFields.contains(p)) availablePoints.add(p);
+            if(!availablePoints.contains(p) && !randomPoints.contains(p) && leftFields.contains(p))
+                availablePoints.add(p);
         }
     }
 
@@ -140,64 +121,65 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
         return list;
     }
 
-    private Point getRandomPointFromLeftFields(ArrayList<Point> randomPoints){
-        Point p = null;
-        while(p == null){
-            int index = (int)(Math.random()*leftFields.size());
-            if(!(randomPoints.contains(leftFields.get(index)))) p = leftFields.get(index);
-        }
-
-        return p;
+    private Point getRandomPointFromLeftFields(){
+        return leftFields.get((int) (Math.random() * leftFields.size()));
     }
 
     private Point getLeftPoint(Point p){
-        return getPoint(p.x-1,p.y);
+        return getPoint(p.x - 1, p.y);
     }
 
     private Point getRightPoint(Point p){
-        return getPoint(p.x+1,p.y);
+        return getPoint(p.x + 1, p.y);
     }
 
     private Point getTopPoint(Point x){
-        return getPoint(x.x,x.y+1);
+        return getPoint(x.x, x.y + 1);
     }
 
     private Point getBottomPoint(Point x){
-        return getPoint(x.x,x.y-1);
+        return getPoint(x.x, x.y - 1);
     }
 
     private Point getTopLeftPoint(Point x){
-        return getPoint(x.x-1,x.y+1);
+        return getPoint(x.x - 1, x.y + 1);
     }
 
     private Point getTopRightPoint(Point x){
-        return getPoint(x.x+1,x.y+1);
+        return getPoint(x.x + 1, x.y + 1);
     }
 
     private Point getBottomLeftPoint(Point x){
-        return getPoint(x.x-1,x.y-1);
+        return getPoint(x.x - 1, x.y - 1);
     }
 
     private Point getBottomRightPoint(Point x){
-        return getPoint(x.x+1,x.y-1);
+        return getPoint(x.x + 1, x.y - 1);
     }
 
 
     private Point getPoint(int x, int y){
         if(x >= 0 && x < mapSize && y >= 0 && y < mapSize){
-            return map.get(x*mapSize+y);
+            return map.get(x * mapSize + y);
         }
+
         return null;
     }
 
-    private ArrayList<Point> makeListWithWaterPoints(int size) {
+    private ArrayList<Point> makeListWithWaterPoints(int size){
         var list = new ArrayList<Point>();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
                 list.add(new Point(i, j, '.'));
             }
         }
+
         return list;
+    }
+
+    private void initLeftFields(){
+        leftFields = new ArrayList<>();
+        leftFields.addAll(map);
     }
 
     private String convertMapToString(){
@@ -212,35 +194,36 @@ public class BattleshipGeneratorImpl implements BattleshipGenerator{
     private void displayMap(){
         for(int i = 0; i < mapSize; i++){
             for(int j = 0; j < mapSize; j++){
-                System.out.print(getPoint(i,j).sign);
+                System.out.print(getPoint(i, j).sign);
             }
             System.out.println();
         }
     }
 
-    class Point {
+
+    class Point{
         final int x, y;
         char sign;
 
-        Point(int x, int y, char sign) {
+        Point(int x, int y, char sign){
             this.x = x;
             this.y = y;
             this.sign = sign;
         }
 
         @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+        public boolean equals(Object o){
+            if(this == o) return true;
+            if(o == null || getClass() != o.getClass()) return false;
 
             Point point = (Point) o;
 
-            if (x != point.x) return false;
+            if(x != point.x) return false;
             return y == point.y;
         }
 
         @Override
-        public String toString() {
+        public String toString(){
             return "(" + x + "," + y + ")";
         }
 
