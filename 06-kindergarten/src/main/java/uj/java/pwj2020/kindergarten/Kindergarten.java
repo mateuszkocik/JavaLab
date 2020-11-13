bdf6f0ffc30af484d9345916cfa6c4ac885c6da4
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-11-13 16:28:19 +0100

    Add 6th exercise

---
+++ 06-kindergarten/src/main/java/uj/java/pwj2020/kindergarten/Kindergarten.java
@@ -0,0 +1,41 @@
+package uj.java.pwj2020.kindergarten;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Kindergarten {
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        final var fileName = args[0];
+        System.out.println("File name: " + fileName);
+        //TODO: read children file, and keep children NOT hungry!
+    }
+
+    private static void init() throws IOException {
+        Files.deleteIfExists(Path.of("out.txt"));
+        System.setErr(new PrintStream(new FileOutputStream("out.txt")));
+        new Thread(Kindergarten::runKindergarden).start();
+    }
+
+    private static void runKindergarden() {
+        try {
+            Thread.sleep(10100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            List<String> errLines = Files.readAllLines(Path.of("out.txt"));
+            System.out.println("Children cries count: " + errLines.size());
+            errLines.forEach(System.out::println);
+            System.exit(errLines.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
