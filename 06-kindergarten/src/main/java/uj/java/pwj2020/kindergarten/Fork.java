fc523c7c7261b8cfd8065d12ee6dbe0182ff0e0b
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-11-29 18:48:39 +0100

    Before change from readwritelock to synchronized

--- 06-kindergarten/src/main/java/uj/java/pwj2020/kindergarten/Fork.java
+++ 06-kindergarten/src/main/java/uj/java/pwj2020/kindergarten/Fork.java
@@ -3,15 +3,20 @@ package uj.java.pwj2020.kindergarten;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import static uj.java.pwj2020.kindergarten.Fork.State.CLEAN;
+
 public class Fork{
 
-    private State state = State.CLEAN;
+    private State state = CLEAN;
     public ReadWriteLock rwl = new ReentrantReadWriteLock(true);
+    public boolean reserved = false;
     public String reservedFor = null;
 
     public void setState(State state){
         this.state = state;
     }
+
+
 
     public State getState(){
         rwl.readLock().lock();
@@ -20,7 +25,26 @@ public class Fork{
         return result;
     }
 
+    public void setReservation(HungryChild child){
+        reserved = true;
+        reservedFor = child.name();
+    }
+
+    public void cancelReservation(){
+        reserved = false;
+        reservedFor = null;
+    }
+
+    public boolean isClear(){
+        return state == CLEAN;
+    }
+
+    public boolean isReserved(){
+        return reserved == true;
+    }
+
+
     enum State{
-        DIRTY, CLEAN, RESERVED
+        DIRTY, CLEAN
     }
 }
--- 06-kindergarten/src/main/java/uj/java/pwj2020/kindergarten/HungryChild.java
+++ 06-kindergarten/src/main/java/uj/java/pwj2020/kindergarten/HungryChild.java
@@ -17,19 +17,62 @@ public class HungryChild extends Child implements Runnable{
         remainingTime = Integer.MAX_VALUE;
     }
 
-
-
-
-
     @Override
     public void run(){
         while(true){
-            remainingTime = getRemainingTime();
-            if(happiness() < 70 && forksAreClean() && leftChild.remainingTime > remainingTime && rightChild.remainingTime > remainingTime){
-                eatAndCleanForks();
+            //remainingTime = getRemainingTime();
+            //boolean neighboursHaveMoreTime = leftChild.remainingTime > remainingTime && rightChild.remainingTime > remainingTime;
+            if(forksAreFree()){
+                if(happiness() < 70){
+                    eatAndCleanForks();
+                }
+            }else{
+                if(areForksReservedForMe()){
+                    eatAndCancelReservation();
+                }else{
+                    if(happiness() < 30){
+                        if(forksAreNotReserved()){
+
+                        }
+                    }
+                }
             }
         }
     }
+
+    private void reserveForks(){
+        lockWriteLockForks();
+        leftFork.setReservation(this);
+        rightFork.setReservation(this);
+        unlockWriteLockForks();
+    }
+
+    private boolean forksAreFree(){
+        return forksAreClean() && forksAreNotReserved();
+    }
+
+    private boolean forksAreNotReserved(){
+        lockReadLockForks();
+        boolean result = leftFork.isReserved() && rightFork.isReserved();
+        unlockReadLockForks();
+        return result;
+    }
+
+
+    private void eatAndCancelReservation(){
+        setStateInForks(DIRTY);
+        eat();
+        cancelReservation();
+        setStateInForks(CLEAN);
+    }
+
+    public boolean areForksReservedForMe(){
+        return leftFork.reserved
+                && rightFork.reserved
+                && leftFork.reservedFor == name()
+                && rightFork.reservedFor == name();
+    }
+
 
     public void eatAndCleanForks(){
         setStateInForks(DIRTY);
@@ -37,21 +80,49 @@ public class HungryChild extends Child implements Runnable{
         setStateInForks(CLEAN);
     }
 
+    public void cancelReservation(){
+        lockWriteLockForks();
+        leftFork.cancelReservation();
+        rightFork.cancelReservation();
+        unlockWriteLockForks();
+    }
+
     public void setStateInForks(Fork.State state){
-        leftFork.rwl.writeLock().lock();
-        rightFork.rwl.writeLock().lock();
+        lockWriteLockForks();
         leftFork.setState(state);
         rightFork.setState(state);
+        unlockWriteLockForks();
+    }
+
+    private void lockReadLockForks(){
+        leftFork.rwl.readLock().unlock();
+        rightFork.rwl.readLock().unlock();
+    }
+
+    private void unlockReadLockForks(){
+        leftFork.rwl.readLock().unlock();
+        rightFork.rwl.readLock().unlock();
+    }
+
+    private void unlockWriteLockForks(){
         leftFork.rwl.writeLock().unlock();
         rightFork.rwl.writeLock().unlock();
     }
 
+    private void lockWriteLockForks(){
+        leftFork.rwl.writeLock().lock();
+        rightFork.rwl.writeLock().lock();
+    }
+
     public boolean forksAreClean(){
-        return leftFork.getState() == CLEAN && rightFork.getState() == CLEAN;
+        lockReadLockForks();
+        boolean result = leftFork.isClear() && rightFork.isClear();
+        unlockReadLockForks();
+        return result;
     }
 
     public int getRemainingTime(){
-        return happiness()*hungerSpeed();
+        return happiness() * hungerSpeed();
     }
 
 }
