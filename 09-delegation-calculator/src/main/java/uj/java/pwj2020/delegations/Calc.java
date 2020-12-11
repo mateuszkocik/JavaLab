8892bae3b1968aaccd629b37046b924ef8e69785
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-12-11 22:44:24 +0100

    Add 9th exercise

---
+++ 09-delegation-calculator/src/main/java/uj/java/pwj2020/delegations/Calc.java
@@ -0,0 +1,11 @@
+package uj.java.pwj2020.delegations;
+
+import java.math.BigDecimal;
+
+public class Calc {
+
+    BigDecimal calculate(String start, String end, BigDecimal dailyRate) {
+        return null;
+    }
+
+}
